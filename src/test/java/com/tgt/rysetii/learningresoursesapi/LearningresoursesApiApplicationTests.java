65537a7b735e3ae6d8f179b25e8184fdefe6ce83
Sandra-Anna <78840610+Sandra-Anna@users.noreply.github.com>
2022-07-09 22:13:41 +0530

    Hello World

---
+++ src/test/java/com/tgt/rysetii/learningresoursesapi/LearningresoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresoursesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
