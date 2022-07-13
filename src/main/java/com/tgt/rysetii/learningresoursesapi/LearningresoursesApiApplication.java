c7d1fe8d2d0a264689db97231ccba108418f255c
Sandra-Anna <78840610+Sandra-Anna@users.noreply.github.com>
2022-07-13 15:46:24 +0530

    2-ClassesAndObjects

--- src/main/java/com/tgt/rysetii/learningresoursesapi/LearningresoursesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresoursesapi/LearningresoursesApiApplication.java
@@ -8,7 +8,6 @@ public class LearningresoursesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresoursesApiApplication.class, args);
-		System.out.println("Hello World!!");
 	}
 
 }
